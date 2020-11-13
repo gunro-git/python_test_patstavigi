59bf95dcec1238964a596d7fecf3741c0e88ba35
gunro-git <53829566+gunro-git@users.noreply.github.com>
2020-11-13 16:52:38 +0200

    Update b_test.py

--- test/b_test.py
+++ test/b_test.py
@@ -2,10 +2,10 @@ import pytest
 from src.u_laukums import laukums
 
 def test_vesel_pozitivi():
-    assert laukums(2, 3) == 2
+    assert laukums(2, 3) == 3
 
 def test_poz_dala():
     assert laukums(2,4.2) == 4.2
 
 def test_abi_dalas():
-    assert laukums(3.2,4.4)==7.04
+    assert round(laukums(3.2,4.4),2)==7.04
